0b12287f0c7b6729a251283fd6743a61c01a4d0b
maggie-bingham <mbingham4309@gmail.com>
2015-08-31 15:50:28 -0400

    linkedin api

--- Pena/config/initializers/devise.rb
+++ Pena/config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :linkedin, "77t3hm8wfwmvfz", "ZKPw8AbOoysBTBgF"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
