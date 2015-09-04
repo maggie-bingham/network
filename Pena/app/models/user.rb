18686a09638359c504c594b021b684301e7b4629
Emanuel <rhodesemanuel@yahoo.com>
2015-09-04 17:24:15 -0400

    email

--- Pena/app/models/user.rb
+++ Pena/app/models/user.rb
@@ -2,17 +2,22 @@ class User < ActiveRecord::Base
 
     def self.from_omniauth(auth)
       Rails.logger.info auth.to_yaml
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.access_token = auth.credentials.token
       user.name = auth.info.name
       user.image_url = auth.info.image
+      user.email = auth.extra.raw_info.emailAddress
+      user.industry = auth.extra.raw_info.industry
+      user.headline = auth.extra.raw_info.headline
       user.save!
       user
       end
     end
 
+
+
     def linkedin_profile
-      api = LinkedIn::API.new(@user.access_token)
+      api = LinkedIn::API.new(access_token)
       api.profile
     end
 
