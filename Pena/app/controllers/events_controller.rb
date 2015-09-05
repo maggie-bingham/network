cd1020f457e769a97926944d831b1204879913eb
Emanuel <rhodesemanuel@yahoo.com>
2015-09-05 16:24:56 -0400

    Meetup Api
    Pulling logic in

--- Pena/app/controllers/events_controller.rb
+++ Pena/app/controllers/events_controller.rb
@@ -31,7 +31,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = Event.new
 
     respond_to do |format|
       if @event.save
@@ -75,8 +75,8 @@ class EventsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:name, :location, :description, :city, :state, :zipcode, :time, :date)
+    def params
+      params.require(:state).permit(:name, :location, :description, :city, :state, :zipcode, :time, :date)
     end
 
 
