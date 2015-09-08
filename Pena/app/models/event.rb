25842907b951d8d507bec8e64973928c9b2a8afb
Emanuel <rhodesemanuel@yahoo.com>
2015-09-08 19:39:10 -0400

    Nevermind

--- Pena/app/models/event.rb
+++ Pena/app/models/event.rb
@@ -20,11 +20,13 @@ class Event < ActiveRecord::Base
   end
 
   def self.results
-    event_data = JSON.parse(MeetupApi.new.open_events(self.param)["results"])
+    meetup = MeetupApi.new.open_events(self.param)
+    event_data = (meetup)["results"]
     event = event_data.map do |event|
       u = Event.new
       u.external_id = event["id"]
       u.event_name = event.slice["group"]["name"]
+    end
   end
 
 
